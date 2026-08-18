8c74a51f8eaebc6b56f88c985ea43a2f6f9c7fd2
Sakshi Maurya <msakshi16264@gmail.com>
2026-08-18 19:54:21 +0530

    Reverse Star Pattern

---
+++ Day_04/Challenge/Challenge2.java
@@ -0,0 +1,13 @@
+package Day_04.Challenge;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        for ( int i = 1; i <= 5; i++) {
+            for ( int j = 5; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
