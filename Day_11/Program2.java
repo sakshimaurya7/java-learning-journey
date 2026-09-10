7c1a29493c5ac885151b508081fa096c131794e8
Sakshi Maurya <msakshi16264@gmail.com>
2026-09-10 22:03:02 +0530

    Add Animal, Dog, and Cat classes with Program2 to demonstrate method overriding

---
+++ Day_11/Program2.java
@@ -0,0 +1,33 @@
+package Day_11;
+
+//Method Overriding Example
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override 
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override 
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Program2 {
+    public static void main(String[] args) {
+        Animal animal;
+        animal = new Dog();
+        animal.sound();
+
+        animal = new Cat();
+        animal.sound();
+    }
+}
