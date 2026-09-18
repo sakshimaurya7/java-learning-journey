9378b8cb27e993489119d80b598bd42498a24174
Sakshi Maurya <msakshi16264@gmail.com>
2026-09-18 19:24:45 +0530

    Add Camera and MusicPlayer interfaces with Smartphone class implementation in Program3 to demonstrate interface usage and method overriding

---
+++ Day_13/Program3.java
@@ -0,0 +1,29 @@
+package Day_13;
+
+interface Camera {
+    void takePhoto();
+}
+
+interface MusicPlayer {
+    void playMusic();
+}
+
+class Smartphone implements Camera, MusicPlayer {
+    @Override 
+    public void takePhoto() {
+        System.out.println("Taking Photo.");
+    }
+
+    @Override 
+    public void playMusic() {
+        System.out.println("Playing music.");
+    }
+}
+
+public class Program3 {
+    public static void main(String[] args) {
+        Smartphone phone = new Smartphone();
+        phone.takePhoto();
+        phone.playMusic();
+    }    
+}
