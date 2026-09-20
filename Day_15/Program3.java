3ef57b3e7e72c008a517ec8035cd61bd3fb280ed
Sakshi Maurya <msakshi16264@gmail.com>
2026-09-20 21:49:11 +0530

    Add Program3 to demonstrate custom exception handling for age validation

---
+++ Day_15/Program3.java
@@ -0,0 +1,26 @@
+package Day_15;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class Program3 {
+    static void checkAge(int age) throws InvalidAgeException {
+        if(age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above.");
+        }
+
+        System.out.println("Eligible to vote.");
+    }    
+
+    public static void main(String[] args) {
+        try {
+            checkAge(16);
+        }
+        catch(InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
